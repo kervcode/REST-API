8fa356b109e84fca889ff35ed538459d2b6c2080
Kervcodes-desktop <Kervintz84@hotmail.com>
2020-09-20 13:28:04 -0400

    added authentication package

--- routes.js
+++ routes.js
@@ -3,16 +3,58 @@
 const express = require("express");
 const { sequelize, User, Course } = require("./models");
 const bcryptjs = require("bcryptjs");
+const auth = require("basic-auth");
 
 const router = express.Router();
+
+// authentication middleware
+const authenticateUser = async (req, res, next) => {
+  const credentials = auth(req);
+
+  if (credentials) {
+    const user = await User.find(u.emailAddress === credentials.emailAddress);
+
+    // If user exist, compare user.password with credentials.password
+    if (user) {
+      const authenticated = bcryptjs.compareSync(
+        credentials.password,
+        user.password
+      );
+      // If a password match is found
+      if (authenticated) {
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.username}`;
+      }
+    } else {
+      message: `Authentication failure for User : ${user.credentials}`;
+    }
+  } else {
+    message = "Authentication not found";
+  }
+
+  if (message) {
+    console.warn(message);
+
+    // Return a response with a 401 Unauthorized HTTP status code.
+    res.status(401).json({ message: "Access Denied" });
+  } else {
+    // Or if user authentication succeeded...
+    // Call the next() method.
+    next();
+  }
+};
 
 /** CREATING THE USER ROUTES */
 
 // returning the currently authenticated user
-router.get("/users", async (req, res, next) => {
-  const users = await User.findAll();
+router.get("/users", authenticateUser, async (req, res, next) => {
+  const user = await req.currentUser;
 
-  res.json(users.map((user) => user.get({ plain: true })));
+  res.json({
+    username: user.emailAddress,
+    password: user.password,
+  });
 });
 
 // create a new user
